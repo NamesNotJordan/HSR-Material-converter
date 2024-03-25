626f1eeb67b8873976374873f78d54423936a7ea
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-25 14:42:26 +0200

    took out class as it is unneeded

--- converter.py
+++ converter.py
@@ -4,23 +4,23 @@ import math
 PURPLE_EXP = 20000
 BLUE_EXP = 5000
 GREEN_EXP = 1000
-class Converter():
-    # Converts number of blue and greenbooks into equivalent number of purple books
-    @staticmethod
-    def upscaleBooks(blueBooks, greenBooks):
-        blue = blueBooks*BLUE_EXP
-        green = greenBooks*GREEN_EXP
-        return math.floor((blue+green)/PURPLE_EXP)
-    
-    
-    # returns number of 
-    @staticmethod
-    def findRequiredBooks(target):
-        targetExp = target * PURPLE_EXP
-        blueRequired = math.floor(targetExp/BLUE_EXP)
-        greenRequired = math.ceil((targetExp%BLUE_EXP)/GREEN_EXP)
-        return (blueRequired, greenRequired)
-    
-    @staticmethod
-    def upscaleTrace(blueMat, greenMat):
-        pass
+
+# Books
+# Converts number of blue and greenbooks into equivalent number of purple books
+def upscaleBooks(blueBooks, greenBooks):
+    blue = blueBooks*BLUE_EXP
+    green = greenBooks*GREEN_EXP
+    return math.floor((blue+green)/PURPLE_EXP)
+
+
+# returns number of 
+def findRequiredBooks(target):
+    targetExp = target * PURPLE_EXP
+    blueRequired = math.floor(targetExp/BLUE_EXP)
+    greenRequired = math.ceil((targetExp%BLUE_EXP)/GREEN_EXP)
+    return (blueRequired, greenRequired)
+
+
+# Traces
+def upscaleTrace(blueMat, greenMat):
+    pass
